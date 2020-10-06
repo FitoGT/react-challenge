0ab6a0d0731da8c9099b9597c32de6668f2deb8b
Adolfo Guevara <aguevara@kiusys.com>
2020-10-06 19:31:50 -0300

    validations in create product

--- src/views/pages/Products/Create.js
+++ src/views/pages/Products/Create.js
@@ -1,54 +1,62 @@
 import React, { useState } from 'react';
-import { Button, Row, FormGroup, Label, Input, Col } from 'reactstrap';
-import { addProduct } from './../../../actions'
-import { useDispatch } from 'react-redux'
+import { Button, Row, FormGroup, Label, Input, Col, Alert } from 'reactstrap';
+import { addProduct } from './../../../actions';
+import { useDispatch } from 'react-redux';
 function Create(props) {
-  const dispatch = useDispatch()
-
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-  const [image, setImage] = useState('')
+  const dispatch = useDispatch();
+  const [error, setError] = useState(false);
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [image, setImage] = useState('');
   let create = () => {
-    if(name.length > 0 && price.length > 0 && image.length > 0){
-      dispatch(addProduct({name,price,image}))
-      props.showCreateForm(false)
-    }else{
+    if (name.length > 0 && price.length > 0 && image.length > 0) {
+      dispatch(addProduct({ name, price, image }));
+      props.showCreateForm(false);
+      setError(false);
+    } else {
+      setError(true);
     }
-  }
+  };
   return (
-
     <Row>
       <Col sm="12" className="mb-5">
         <h3>Create your Product</h3>
       </Col>
-      <Col lg="6"  sm="12">
+      <Col lg="6" sm="12">
         <FormGroup>
           <Label for="name">Name</Label>
-          <Input type="text" name="name" id="name" value={name} onChange={e => setName(e.target.value)}/>
+          <Input type="text" name="name" id="name" value={name} onChange={e => setName(e.target.value)} />
         </FormGroup>
       </Col>
-      <Col lg="6"  sm="12">
+      <Col lg="6" sm="12">
         <FormGroup>
           <Label for="examplePassword">Price</Label>
-          <Input type="number" name="price" id="price"  value={price} onChange={e => setPrice(e.target.value)}/>
+          <Input type="number" name="price" id="price" value={price} onChange={e => setPrice(e.target.value)} />
         </FormGroup>
       </Col>
-      <Col lg="6"  sm="12">
+      <Col lg="6" sm="12">
         <FormGroup>
           <Label for="image">Image Url</Label>
-          <Input type="text" name="text" id="image" value={image} onChange={e => setImage(e.target.value)}/>
+          <Input type="text" name="text" id="image" value={image} onChange={e => setImage(e.target.value)} />
         </FormGroup>
       </Col>
+      {error ? (
+        <Col sm="12">
+          <Alert color="danger">Please complete all the fields!</Alert>
+        </Col>
+      ) : null}
       <Col sm="12">
         <FormGroup>
-            <Button color="primary" onClick={()=>create()}>Save</Button>
-            {' '}
-            <Button color="primary" onClick={()=>props.showCreateForm(false)}>List</Button>
-
+          <Button color="primary" onClick={() => create()}>
+            Save
+          </Button>{' '}
+          <Button color="primary" onClick={() => props.showCreateForm(false)}>
+            List
+          </Button>
         </FormGroup>
       </Col>
     </Row>
-  )
+  );
 }
 
-export default Create
+export default Create;
--- src/views/pages/Products/Update.js
+++ src/views/pages/Products/Update.js
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import { Button, Row, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Button, Row, FormGroup, Label, Input, Col, Alert } from 'reactstrap';
 import { updateProduct } from './../../../actions';
 import { useDispatch, useSelector } from 'react-redux';
 function Update(props) {
   let products = useSelector(state => state.products);
-
+  const [error, setError] = useState(false);
   const dispatch = useDispatch();
-
   const [name, setName] = useState(products[props.productKey].name);
   const [price, setPrice] = useState(products[props.productKey].price);
   const [image, setImage] = useState(products[props.productKey].image);
   let update = () => {
     if (name.length > 0 && price.length > 0 && image.length > 0) {
-
-      dispatch(updateProduct({ id:props.productKey, name, price, image }));
-      props.showUpdateForm({id:props.productKey,show:false});
+      dispatch(updateProduct({ id: props.productKey, name, price, image }));
+      props.showUpdateForm({ id: props.productKey, show: false });
+      setError(false);
     } else {
+      setError(true);
     }
   };
   return (
@@ -41,6 +41,11 @@ function Update(props) {
           <Input type="text" name="text" id="image" value={image} onChange={e => setImage(e.target.value)} />
         </FormGroup>
       </Col>
+      {error ? (
+        <Col sm="12">
+          <Alert color="danger">Please complete all the fields!</Alert>
+        </Col>
+      ) : null}
       <Col sm="12">
         <FormGroup>
           <Button color="primary" onClick={() => update()}>
